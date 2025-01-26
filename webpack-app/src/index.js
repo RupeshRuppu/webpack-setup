b5308e1551855d5d5ae822e3a4cbff90e13744ef
Rupesh Lakshman <codewithruppu@gmail.com>
2025-01-26 07:17:38 +0530

    css file getting added by js

--- webpack-app/src/index.js
+++ webpack-app/src/index.js
@@ -1,3 +1,4 @@
+import "./main.css";
 import { AlertService } from "./app/alert.service";
 import { ComponentService } from "./app/component.service";
 import { run } from "./app/app";
--- webpack-app/webpack.config.js
+++ webpack-app/webpack.config.js
@@ -7,4 +7,12 @@ module.exports = {
 		filename: "bundle.js",
 		path: path.resolve(__dirname, "build"),
 	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: ["style-loader", "css-loader"],
+			},
+		],
+	},
 };
