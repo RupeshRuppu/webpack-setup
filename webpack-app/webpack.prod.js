282fc389061bb805ff716d34d52a9368f1e45919
Rupesh Lakshman <codewithruppu@gmail.com>
2025-01-26 11:18:11 +0530

    splitted main bundle and vendor.

--- webpack-app/webpack.prod.js
+++ webpack-app/webpack.prod.js
@@ -6,7 +6,7 @@ const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 module.exports = merge(config, {
 	mode: "production",
 	output: {
-		filename: "bundle.[contenthash].js",
+		filename: "[name].[contenthash].bundle.js",
 		path: path.resolve(__dirname, "build"),
 		assetModuleFilename: "images/[hash][ext][query]",
 	},
