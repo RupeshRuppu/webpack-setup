73b087663a6232be62a8f35e4d04589604f7fc75
Rupesh Lakshman <codewithruppu@gmail.com>
2025-01-26 07:32:23 +0530

    sass-loader with sass as dependency

--- webpack-app/src/index.js
+++ webpack-app/src/index.js
@@ -1,4 +1,4 @@
-import "./main.css";
+import "./main.scss";
 import { AlertService } from "./app/alert.service";
 import { ComponentService } from "./app/component.service";
 import { run } from "./app/app";
