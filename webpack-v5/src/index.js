d6fd90d484a7a1bc2f863aa8a08fab87055444a9
Rupesh Lakshman <codewithruppu@gmail.com>
2025-09-20 11:36:40 +0530

    initial webpack setup

---
+++ webpack-v5/src/index.js
@@ -0,0 +1,13 @@
+const rupesh = {
+	name: "rupesh",
+	age: 24,
+	occupation: "software dev",
+};
+
+const rujith = {
+	name: "rujith",
+	age: 24,
+	occupation: "full stack dev",
+};
+
+console.log(rupesh, rujith);
